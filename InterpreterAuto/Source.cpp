28b74d6d1b08d91b06d0cc74533cbfa7b880ed01
P4ll <cornelius.weling@gmail.com>
2018-05-28 13:41:56 +0400

    Добавьте файлы проекта.

---
+++ InterpreterAuto/Source.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//vector<string> rebuildString(string str);
+
+class Trigger {
+	int mod;
+	int number;
+	char *matrix;
+public:
+	Trigger(int n, int m, int mod, int number, char *mat) {
+		
+	}
+};
+
+string getU() {
+	//freopen("w.txt", "r", stdin);
+	string a;
+	cin >> a;
+	int b = 10;
+	return a;
+}
+
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int interpreterCode;
+	scanf("%d", &interpreterCode);
+	string inputData;
+	cin >> inputData;
+	getU();
+	return 0;
+}
+/*
+vector<string> rebuildString(string str) {
+	return;
+}
+*/
